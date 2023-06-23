0911b3af88cd2fadb2164cac986720fd200f7222
minhduc1205-03 <111838502+minhduc1205-03@users.noreply.github.com>
2023-06-23 23:41:19 +0700

    first commit

---
+++ FRONT END/main.js
@@ -0,0 +1,11 @@
+// có ba cách để thêm code css vào trong hmtl
+// cách một đó là internal trong cách này thì ta sẽ thêm một cặp thẻ
+// style vào trong thẻ head ở html sau đó viết code css ở trong cặp thẻ style này
+// cách 2 là external ở cách này chúng ta sẽ tạo ra một file main.css riêng sau đó viết code css vào trong file này
+// sau đó muốn thêm code css vào trong html thì ta thêm một thẻ link ở html
+// <link ref = "stylesheet" href = "main.css"
+// cách thứ3 là inline nghĩa là ta viết trực tiêp css ở trong thẻ
+// ví dú nhu <p style= "width: 100px; height: 100px"trong 3 cách trên thì chúng ta thường tạo ra một file css là main.css rồi thêm css nào vào html bằng cách tạo một link rè="stylesheet" href= "main.css"
+
+
+// #140B5C
